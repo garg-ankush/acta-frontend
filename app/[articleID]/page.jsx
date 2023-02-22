53cb6c530c468c495fef8a14e502f8762d953e89
ankush <unkush-garg@github.com>
2023-02-22 15:35:41 +0530

    Fix css for individual artical page

--- app/[articleID]/page.jsx
+++ app/[articleID]/page.jsx
@@ -49,41 +49,41 @@ export default async function ArticleDetail({params}) {
     const audioDuration = await readingTime(articleDetails.content) || ""
 
     return (
-        <div className="flex bg-white h-screen">
+        <div className="flex bg-white h-screen mx-auto w-[300px] md:w-[800px]">
             {/* Title */}
-            <div className="bg-white rounded-xl">
-                <div className="py-3 m-3 ml-20 flex">
-                    <div className="text-center m-3 flex text-sm items-center justify-center bg-[#E4ECF4] border-2 border-gray text-black h-10 rounded-2xl">
+            <div className="">
+                <div className="py-3 m-3 ml-20">
+                    {/* <div className="text-center m-3 flex text-sm items-center justify-center bg-[#E4ECF4] border-2 border-gray text-black h-10 rounded-2xl">
                         <p className="mt-[0.5rem] m-3 justify-center text-center items-center">Climate</p>
-                    </div>
+                    </div> */}
                     {/* <p className="mt-[1rem] text-gray-500">
                         {date}
                     </p> */}
 
                 </div>
 
-                <div className="justify-center bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 flex w-96 h-16 ml-20 mt-[-1rem] mb-3 rounded-xl gap-4">
-                        <div className="ml-3 justify-center items-center mt-[0.7rem]">
+                <div className="p-2 md:ml-20 md:mr-20 justify-center bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 flex md:w-96 md:h-16 w-72 h-16 mt-[-1rem] mb-3 rounded-xl gap-4">
+                        <div className="ml-3 justify-center items-center">
                             <AudioPlayer audioSourceURL={sourceURL}/>
                         </div>
 
                         <div className="">
-                            <h1 className="font-semibold text-[#FFFFFF] text-2xl mt-[1.1rem]">{audioDuration}-minute listen</h1>
+                            <h1 className="font-semibold text-[#FFFFFF] text-2xl mt-[0.5rem]">{audioDuration}-minute listen</h1>
                         </div>
                 </div>
                 
-                <p className="font-semibold ml-20 mr-20">
+                <p className="font-semibold flex p-2 md:ml-20 md:mr-20">
                  {articleDetails.title}
                 </p>
 
-                <h1 className="ml-20 mr-20 mt-4">
+                <h1 className="p-2 md:ml-20 md:mr-20 mt-4">
                     <p className="font-semibold">
                         <span>Summary</span>
                     </p>
                   {articleDetails.summary}
                 </h1>
 
-                <h1 className="ml-20 mt-4 mr-20">
+                <h1 className="p-2 md:ml-20 md:mr-20 mt-4">
                     <p className="font-semibold">
                         <span>Article</span>
                     </p>
