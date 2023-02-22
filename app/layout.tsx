22c48c4f65b52c44259eb16f25e533a19128fe4c
ankush <unkush-garg@github.com>
2023-02-22 15:36:05 +0530

    Add navigation and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import AboutActa from '../components/AboutActa'
 
 export default function RootLayout({
   children,
@@ -12,7 +13,25 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className='justify-between'>
+        <nav className="bg-[#F1B61B] w-full h-20 items-center justify-center">
+          <div className='text-4xl text-white ml-2'>
+            <span className='underline text-5xl text-white'>act</span>a
+          </div>
+
+          {/* <div className='justify-center items-center'>
+            <AboutActa />
+          </div> */}
+          
+        </nav>
+        </div>
+        {/* Navbar */}
+        
+
+        {children}
+
+      </body>
     </html>
   )
 }
