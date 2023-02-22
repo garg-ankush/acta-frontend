e0b64af64f363d65d2c2451e1832f93e0c4bdb70
ankush <unkush-garg@github.com>
2023-02-22 15:36:26 +0530

    Add about page - not used

---
+++ components/AboutActa.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutActa = () => {
+  return (
+    <div className='absolute right-0 text-2xl text-white'>
+        About Acta
+    </div>
+  )
+}
+
+export default AboutActa
